8403c42b48047620d270249104692de929866d1d
https://github.com/FloBut/Booking-System.git <f.butuc5@gmail.com>
2024-08-16 16:02:38 +0300

    Button

---
+++ src/main/java/com/example/vaadinApp/VaadinAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.vaadinApp;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinAppApplication.class, args);
+	}
+
+}
